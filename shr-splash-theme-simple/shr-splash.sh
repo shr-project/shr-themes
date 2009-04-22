39937b6f86b9f9462b17eba6e548f3819677755a
Julien 'Ainulindalë' Cassignol <ainulindale@gmail.com>
2009-04-22 17:11:33 +0200

    shr-splash-theme-simple: Removed needed "splash" kernel option.

--- shr-splash-theme-simple/shr-splash.sh
+++ shr-splash-theme-simple/shr-splash.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
-if [ "`grep -i splash /proc/cmdline`" ]; then
-  case "$1" in
-      start)
-         [ -f /usr/share/shr-splash/boot.fb ] && cat /usr/share/shr-splash/boot.fb > /dev/fb0
-      ;;
-      stop)
-         [ -f /usr/share/shr-splash/shutdown.fb ] && cat /usr/share/shr-splash/shutdown.fb > /dev/fb0
-      ;;
-  esac
-fi
+case "$1" in
+    start)
+       [ -f /usr/share/shr-splash/boot.fb ] && cat /usr/share/shr-splash/boot.fb > /dev/fb0
+    ;;
+    stop)
+       [ -f /usr/share/shr-splash/shutdown.fb ] && cat /usr/share/shr-splash/shutdown.fb > /dev/fb0
+    ;;
+esac
 
 exit 0
